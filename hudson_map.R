c50648f24e7868b9f142c09091177a224ed16bf6
mhpob <mikeob9@gmail.com>
2016-11-22 12:15:41 -0500

    Used grouped data from a single data frame.

--- hudson_map.R
+++ hudson_map.R
@@ -3,25 +3,25 @@ library(ggplot2); library(rgdal)
 midatl <- readOGR('c:/users/secor/desktop/gis products/chesapeake/midatlantic',
                   'matl_states_land')
 
-recs2015 <- read.csv('p:/obrien/biotelemetry/hudson sb/hudson_receivers_2015.csv',
-                     stringsAsFactors = F)
+plot_points <- read.csv('p:/obrien/biotelemetry/hudson sb/plotting points.csv')
 
 midatl <- fortify(midatl)
 
 hud_s <- ggplot() + geom_polygon(data = midatl, aes(long, lat, group = group),
                                  fill = 'darkgray') +
   coord_map(xlim = c(-74.1, -73.8336), ylim = c(40.5980, 41.1545)) +
-  geom_point(data = recs2015, aes(x = Long.1, y = Lat.1, color = Owner)) +
+  geom_point(data = plot_points, aes(x = Long, y = Lat,
+                                     color = Owner, shape = Owner)) +
   theme_bw() +
   theme(axis.title = element_blank(), legend.position = 'none')
 hud_sm <- ggplot() +
   annotate('rect', xmin = -74.05, xmax = -73.9,
            ymin = 41.32, ymax = 41.52, fill = 'blue') +
-  annotate('point', x = -73.99144, y = 41.44596, fill = 'yellow', pch = 25) +
   geom_polygon(data = midatl, aes(long, lat, group = group),
                fill = 'darkgray') +
   coord_map(xlim = c(-74.1, -73.8336), ylim = c(41.1545, 41.711)) +
-  geom_point(data = recs2015, aes(x = Long.1, y = Lat.1, color = Owner)) +
+  geom_point(data = plot_points, aes(x = Long, y = Lat,
+                                     color = Owner, shape = Owner)) +
   theme_bw() +
   theme(axis.title = element_blank(), legend.position = 'none')
 hud_nm <- ggplot() +
@@ -30,7 +30,8 @@ hud_nm <- ggplot() +
   geom_polygon(data = midatl, aes(long, lat, group = group),
                fill = 'darkgray') +
   coord_map(xlim = c(-74.0176, -73.7512), ylim = c(41.711, 42.2675)) +
-  geom_point(data = recs2015, aes(x = Long.1, y = Lat.1, color = Owner)) +
+  geom_point(data = plot_points, aes(x = Long, y = Lat,
+                                     color = Owner, shape = Owner)) +
   theme_bw() +
   theme(axis.title = element_blank(), legend.position = 'none')
 hud_n <- ggplot() +
@@ -39,7 +40,8 @@ hud_n <- ggplot() +
   geom_polygon(data = midatl, aes(long, lat, group = group),
                fill = 'darkgray') +
   coord_map(xlim = c(-73.8338, -73.5674), ylim = c(42.2675, 42.8240)) +
-  geom_point(data = recs2015, aes(x = Long.1, y = Lat.1, color = Owner)) +
+  geom_point(data = plot_points, aes(x = Long, y = Lat,
+                                     color = Owner, shape = Owner)) +
   theme_bw() +
   theme(axis.title = element_blank(), legend.position = 'none')
 
