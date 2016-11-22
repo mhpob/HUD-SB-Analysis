9d37323fb9de834599aa868dcf96806ef71da98d
mhpob <mikeob9@gmail.com>
2016-11-22 15:55:30 -0500

    Add Albany temperature.

--- musicVtemp.R
+++ musicVtemp.R
@@ -35,16 +35,31 @@ music <- ggplot() + geom_raster(data = plot.data,
 ## VR2AR Data
 vr2ar <- read.csv('p:/obrien/biotelemetry/hudson sb/hudson_vr2ar_events.csv',
                   stringsAsFactors = F)
-vr2ar <- mutate(vr2ar, Date.Time = ymd_hms(Date.Time))
+vr2ar <- filter(vr2ar, Description == 'Average temperature') %>%
+  mutate(Date.Time = ymd_hms(Date.Time),
+         temp = as.numeric(Data),
+         Region = 'Lower') %>%
+  group_by(Date.Time, Region) %>%
+  summarize(avg.temp = mean(temp, na.rm = T))
+
+## Albany USGS Temperature
+alb <- read.csv('p:/obrien/biotelemetry/hudson sb/albanytemp.csv')
+alb <- mutate(alb, date.time = ymd_hms(date.time),
+         Date.Time = ceiling_date(date.time, unit = 'hours'),
+         Region = 'Upper') %>%
+  group_by(Date.Time, Region) %>%
+  summarize(avg.temp = mean(value, na.rm = T))
+
+temp.data <- rbind(vr2ar, alb)
 
 temperature.plot <- ggplot() +
-  geom_point(data = filter(vr2ar, Description == 'Average temperature'),
-             aes(x = Date.Time, y = as.numeric(Data))) +
+  geom_line(data = temp.data, aes(x = Date.Time, y = avg.temp, color = Region)) +
   labs(y = 'Temp') +
   theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
         axis.title.x = element_blank()) +
   lims(x = c(ymd_hms('2016-05-19 00:00:00'), ymd_hms('2016-06-16 00:00:00')),
-       y = c(14,22))
+       y = c(14,25)) +
+  theme(legend.position = 'none')
 
 library(gridExtra)
 grid.arrange(temperature.plot, music, heights = c(1,3))
