28724a5b03c9026ac7055049c83b9696bcde529e
mhpob <mikeob9@gmail.com>
2016-12-07 12:02:00 -0500

    Create returnsVtime.R.

---
+++ returnsVtime.R
@@ -0,0 +1,18 @@
+library(lubridate); library(dplyr)
+
+source('detection_input.R')
+
+agg_detects <- filter(detects, !is.na(date.local)) %>%
+  mutate(week = ceiling_date(date.local, unit = 'week')) %>%
+  group_by(week, Region, array) %>%
+  distinct(trans.num) %>%
+  summarize(count = n())
+
+library(ggplot2)
+ggplot() + geom_step(data = agg_detects,
+                     aes(x = week, y = count/50, color = Region),
+                     lwd = 1) +
+  scale_x_datetime(limits = c(ymd_hms('2016-04-19 00:00:00'),
+                              ymd_hms('2016-07-05 00:00:00'))) +
+  facet_wrap(~ array, ncol = 1)
+
