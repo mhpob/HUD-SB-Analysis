99d17967b39a5beef6f33c904104de5dee51ced9
mhpob <mikeob9@gmail.com>
2016-12-07 12:01:21 -0500

    Change "lower" and "upper" regions to cities.

--- detection_input.R
+++ detection_input.R
@@ -3,12 +3,18 @@ sb <- read.csv('p:/obrien/biotelemetry/hudson sb/sb sonic tags 2016.csv',
 sb <- dplyr::mutate(sb, TL = TL/100,
                     FL = FL/100,
                     Weight = Weight/1000,
-                    Region = ifelse(Location == 'RM 59', 'Lower', 'Upper'))
+                    Region = ifelse(Location == 'RM 59',
+                                    'West Point-Newburgh',
+                                    'Saugerties-Coxsackie'))
 sb <- reshape2::melt(sb, id.vars = c('Date','Batch', 'Location', 'Region',
                                      'Gear', 'Sex', 'Stage', 'Transmitter'),
                      measure.vars = c('TL', 'FL', 'Weight'))
+
 arrays <- read.csv('p:/obrien/biotelemetry/hudson sb/receiver arrays.csv',
                    stringsAsFactors = F)
+arrays$array <- factor(arrays$array, ordered = T,
+                  levels = c('Above', 'Saugerties-Coxsackie', 'Between',
+                             'West Point-Newburgh', 'Below'))
 
 detects <- TelemetryR::vemsort('p:/obrien/biotelemetry/hudson sb/receiver logs')
 detects <- dplyr::filter(detects, transmitter %in%
