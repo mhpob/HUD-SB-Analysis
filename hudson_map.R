7c1e446a824dcf8c06ac737d9f06408552bc8e2d
mhpob <mikeob9@gmail.com>
2018-02-07 14:29:37 -0500

    Switch from rgdal to sf.

--- hudson_map.R
+++ hudson_map.R
@@ -1,57 +1,54 @@
-library(ggplot2); library(rgdal)
+library(ggplot2); library(sf) #Dev version of ggplot2 as of 20170207
 
-midatl <- readOGR('c:/users/secor/desktop/gis products/chesapeake/midatlantic',
-                  'matl_states_land')
+midatl <- st_read('c:/users/secor/desktop/gis products/chesapeake/midatlantic')
 
-plot_points <- read.csv('p:/obrien/biotelemetry/hudson sb/points_2016.csv')
+plot_points <- read.csv('p:/obrien/biotelemetry/hudson sb/reports/points_2016.csv')
 
-midatl <- fortify(midatl)
-
-hud_s <- ggplot() + geom_polygon(data = midatl, aes(long, lat, group = group),
-                                 fill = 'darkgray') +
-  coord_map(xlim = c(-74.1, -73.8336), ylim = c(40.5980, 41.13725)) +
+hud_s <- ggplot() + geom_sf(data = midatl, fill = 'darkgray') +
+  coord_sf(xlim = c(-74.1, -73.8336), ylim = c(40.5980, 41.13725), expand = F) +
   scale_x_continuous(breaks = c(-74.0, -73.9)) +
   geom_point(data = plot_points, aes(x = Long, y = Lat,
                                      shape = Type), size = 3.5) +
   scale_shape(solid = F) +
   theme_bw() +
-  theme(axis.title = element_blank(), legend.position = 'none')
+  theme(axis.title = element_blank(), legend.position = 'none',
+        panel.grid.major = element_line(color = 'white'))
 hud_sm <- ggplot() +
   annotate('rect', xmin = -74.05, xmax = -73.9,
            ymin = 41.32, ymax = 41.52, fill = 'lightblue') +
-  geom_polygon(data = midatl, aes(long, lat, group = group),
-               fill = 'darkgray') +
-  coord_map(xlim = c(-74.1, -73.8336), ylim = c(41.13725, 41.67650)) +
+  geom_sf(data = midatl, fill = 'darkgray') +
+  coord_sf(xlim = c(-74.1, -73.8336), ylim = c(41.13725, 41.67650), expand = F) +
   scale_x_continuous(breaks = c(-74.0, -73.9)) +
   geom_point(data = plot_points, aes(x = Long, y = Lat,
                                      shape = Type), size = 3.5) +
   scale_shape(solid = F) +
   theme_bw() +
-  theme(axis.title = element_blank(), legend.position = 'none')
+  theme(axis.title = element_blank(), legend.position = 'none',
+        panel.grid.major = element_line(color = 'white'))
 hud_nm <- ggplot() +
   annotate('rect', xmin = -74, xmax = -73.75,
            ymin = 42.07, ymax = 42.36, fill = 'pink') +
-  geom_polygon(data = midatl, aes(long, lat, group = group),
-               fill = 'darkgray') +
-  coord_map(xlim = c(-74.0664, -73.8), ylim = c(41.67650, 42.21575)) +
+  geom_sf(data = midatl, fill = 'darkgray') +
+  coord_sf(xlim = c(-74.0664, -73.8), ylim = c(41.67650, 42.21575), expand = F) +
   scale_x_continuous(breaks = c(-74.0, -73.9)) +
   geom_point(data = plot_points, aes(x = Long, y = Lat,
                                      shape = Type), size = 3.5) +
   scale_shape(solid = F) +
   theme_bw() +
-  theme(axis.title = element_blank(), legend.position = 'none')
+  theme(axis.title = element_blank(), legend.position = 'none',
+        panel.grid.major = element_line(color = 'white'))
 hud_n <- ggplot() +
   annotate('rect', xmin = -74, xmax = -73.75,
            ymin = 42.07, ymax = 42.36, fill = 'pink') +
-  geom_polygon(data = midatl, aes(long, lat, group = group),
-               fill = 'darkgray') +
-  coord_map(xlim = c(-73.8914, -73.625), ylim = c(42.21575, 42.75500)) +
+  geom_sf(data = midatl, fill = 'darkgray') +
+  coord_sf(xlim = c(-73.8914, -73.625), ylim = c(42.21575, 42.75500), expand = F) +
+  scale_x_continuous(breaks = c(-73.8, -73.7)) +
   geom_point(data = plot_points, aes(x = Long, y = Lat,
                                      shape = Type), size = 3.5) +
   scale_shape(solid = F) +
   theme_bw() +
-  theme(axis.title = element_blank(), legend.position = 'none')
+  theme(axis.title = element_blank(), legend.position = 'none',
+        panel.grid.major = element_line(color = 'white'))
 
 library(gridExtra)
-grid.arrange(hud_n, hud_nm, hud_sm, hud_s,
-             nrow = 1, left = 'Latitude', bottom = 'Longitude')
+grid.arrange(hud_n, hud_nm, hud_sm, hud_s, nrow = 1)
