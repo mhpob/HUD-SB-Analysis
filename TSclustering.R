e690ed83027f0b8924e786e6fbe004e9e2a07069
mhpob <mikeob9@gmail.com>
2018-02-23 15:59:16 -0500

    Cluster comparison and plotting.

--- TSclustering.R
+++ TSclustering.R
@@ -1,18 +1,69 @@
-
 library(dtwclust)
 
 source('perfishTS.R')
 
-k <- reshape2::dcast(agg.pad.imp, Transmitter ~ date.floor, value.var = 'avg.imp')
-row.names(k) <- k$Transmitter
-k <- as.matrix(k[, -1])
+# agg.pad.imp <- agg.pad.imp %>%
+#   group_by(Transmitter) %>%
+#   mutate(avg.scale = scale(avg.imp, center = 40.85, scale = sd(avg.imp)))
+# test <- split(agg.pad.imp, agg.pad.imp$Transmitter)
 
-k <- tslist(k)
+# Put time series into rows ----
+r_series <- reshape2::dcast(agg.pad.imp, Transmitter ~ date.floor, value.var = 'avg.imp')
+row.names(r_series) <- r_series$Transmitter
+r_series <- as.matrix(r_series[, -1])
 
 
-l <- tsclust(k, k = 2, distance = 'sbd', centroid = 'shape')
-plot(l)
-ord <- l$order
+clus.eval <- function(data, n, dist, cent, window){
+  clus <- tsclust(series = data, k = n, distance = dist, centroid = cent,
+                  window.size = window, trace = T,
+                  control = partitional_control(pam.precompute = FALSE,
+                                                iter.max = 500))
+
+  print(clus)
+
+  plot(clus) + ylim(40.8, 42.75) +
+    annotate('rect', xmin = 0, xmax = 86,
+             ymin = 42.07, ymax = 42.36, fill = 'pink', alpha = 0.4) +
+    annotate('rect', xmin = 0, xmax = 86,
+             ymin = 41.32, ymax = 41.52, fill = 'lightblue', alpha = 0.4)
+
+  clus
+}
+
+c2 <- clus.eval(r_series, 2, 'dtw_basic', 'median', '7')
+c3 <- clus.eval(r_series, 3, 'dtw_basic', 'median', '7')
+c4 <- clus.eval(r_series, 4, 'dtw_basic', 'median', '7')
+c5 <- clus.eval(r_series, 5, 'dtw_basic', 'median', '7')
+
+sapply(list(K2 = c2, K3 = c3, K4 = c4, K5 = c5), cvi,
+       type = 'internal')
+
+cents <- data.frame(cent = rep(c('Centroid 1', 'Centroid 2'), each = 86),
+                    value = c(c2@centroids[[1]], c2@centroids[[2]]),
+                    date = rep(unique(agg.pad.imp$date.floor), times = 2))
+
+TS <- data.frame(TS = do.call(c, c2@datalist),
+                 trans = rep(names(c2@datalist), each = 86),
+                 date = rep(unique(agg.pad.imp$date.floor), times = 66),
+                 cent = paste0('Centroid ', rep(c2@cluster, each = 86)))
+
+ggplot(cents) +
+  facet_wrap(~cent) + ylim(40.8, 42.75) +
+  annotate('rect', xmin = ymd_hms('2017-04-02 00:00:00'),
+           xmax = ymd_hms('2017-06-24 00:00:00'),
+           ymin = 42.07, ymax = 42.36, fill = 'pink') +
+  annotate('rect', xmin = ymd_hms('2017-04-02 00:00:00'),
+           xmax = ymd_hms('2017-06-24 00:00:00'),
+           ymin = 41.32, ymax = 41.52, fill = 'lightblue') +
+  geom_line(data = TS, aes(x = date, y = TS, group = trans), color = 'gray') +
+  geom_line(aes(x = date, y = value), lwd = 1.5) +
+  labs(x = NULL, y = 'Latitude') +
+  theme_bw()
+
+# slotNames()
+# [1] "iter"      "converged" "clusinfo"  "cldist"    "call"      "family"    "control"   "datalist"
+# [9] "type"      "distance"  "centroid"  "preproc"   "k"         "cluster"   "centroids" "distmat"
+# [17] "proctime"  "dots"      "args"      "seed"
 
 plot.data <- agg.pos.imp %>%
   ungroup() %>%
@@ -33,7 +84,7 @@ ggplot() + geom_raster(data = plot.data, aes(x = date.floor, y = Transmitter,
 
 
 
-
+frechet_func <- function(x, y){distFrechet()}
 
 
 
@@ -43,9 +94,14 @@ row.names(k) <- k$Transmitter
 k <- as.matrix(k[, -1])
 
 library(kmlShape)
+
+pr_DB$set_entry(FUN = distFrechet, names = c('Frechet'),
+                loop = T, type = 'metric', distance = T)
+
+
 p <- cldsWide(k, id = rownames(k))
 
-kmlclus <- kmlShape(p, 2, timeScale = 3, toPlot = 'none')
+kmlclus <- kmlShape(p, 2, timeScale = 0.03, toPlot = 'none')
 plot(kmlclus, col = 'clusters')
 
 
