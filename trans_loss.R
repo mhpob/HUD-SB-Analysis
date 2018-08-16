30a324bdd1f39ebbad3a154878e7a10b33dd1d50
mhpob <mikeob9@gmail.com>
2018-08-16 17:49:37 -0400

    Extra plotting.

--- trans_loss.R
+++ trans_loss.R
@@ -8,9 +8,10 @@ ggplot() +
   scale_x_datetime(date_breaks = '3 month',
                    date_labels = '%b %Y',
                    limits = c(ymd_hms('2016-05-01 00:00:00'),
-                              ymd_hms('2018-05-01 00:00:00'))) +
+                              ymd_hms('2017-12-01 00:00:00'))) +
   theme_bw() +
-  theme(legend.position = c(0.3, 0.3))
+  theme(axis.text = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
 
 # Loss coded by tagging-location-based classification
 loss_tag <- detects %>%
@@ -20,7 +21,16 @@ loss_tag <- detects %>%
   mutate(cl_type = 'tagging')
 
 # Loss coded by cluster-based classification
-# "temp" is from TSclustering.R. Need to change this name.
+# Load saved c2 from TSclustering.R.
+c2 <- readRDS('cluster data/c2.rda')
+temp <- data.frame(transmitter = names(c2[[1]]$results[[1]]@datalist),
+                   cluster = c2[[1]]$results[[1]]@cluster)%>%
+  left_join(distinct(detects, transmitter, region)) %>%
+  mutate(cluster = case_when(cluster == 1 ~ 'Up-River',
+                             T ~ 'Salt Front'),
+         correct = case_when(cluster == region ~ T,
+                             T ~ F))
+
 loss_clust <- detects %>%
   left_join(temp) %>%
   filter(date.local >= '2017-04-01') %>%
@@ -58,7 +68,9 @@ loss_clust <- detects %>%
   split(., .$cluster) %>%
   lapply(trans_loss, 'date.local', 'transmitter') %>%
   bind_rows(.id = 'region') %>%
-  mutate(cl_type = 'cluster') %>%
+  mutate(cl_type = 'cluster',
+         region = factor(region, levels = c('Up-River', 'Salt Front'),
+                         ordered = T)) %>%
   rbind(loss_tag) %>%
   filter(date >= '2017-04-01')
 
@@ -73,3 +85,37 @@ ggplot() +
                               ymd_hms('2018-04-10 00:00:00'))) +
   theme_bw() +
   theme(legend.position = c(0.85, 0.8))
+
+ggplot() +
+  geom_line(data = loss_overall, aes(x = date, y = remaining), lwd = 1.5) +
+  geom_line(data = filter(loss_clust, cl_type == 'cluster'),
+            aes(x = date, y = remaining, color = region), lwd = 1.5) +
+  labs(x = NULL, y = 'Fish Remaining') +
+  scale_color_manual(values = c('hotpink2', 'steelblue2')) +
+  scale_x_datetime(date_breaks = '3 month',
+                   date_labels = '%b %Y',
+                   limits = c(ymd_hms('2016-05-01 00:00:00'),
+                              ymd_hms('2017-12-01 00:00:00'))) +
+  theme_bw() +
+  theme(legend.position = c(0.85, 0.8),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 15),
+        axis.text = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+
+
+ggplot() +
+  geom_line(data = filter(loss_clust, cl_type == 'cluster'),
+            aes(x = date, y = remaining, color = region), lwd = 1.5) +
+  labs(x = NULL, y = 'Fish Remaining') +
+  scale_color_manual(values = c('hotpink2', 'steelblue2')) +
+  scale_x_datetime(date_breaks = '1 month',
+                   date_labels = '%b %Y',
+                   limits = c(ymd_hms('2017-04-01 00:00:00'),
+                              ymd_hms('2017-08-01 00:00:00'))) +
+  theme_bw() +
+  theme(legend.position = c(0.85, 0.8),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 15),
+        axis.text = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
