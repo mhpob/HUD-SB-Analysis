45c6179c3c7af89840d69fd438808a84dc206f19
Mike O'Brien <mikeob9@gmail.com>
2020-09-16 14:10:15 -0400

    Begin addition of map for rev. 2.

--- Manuscript/kneebone_manu.R
+++ Manuscript/kneebone_manu.R
@@ -1,10 +1,9 @@
 library(dplyr)
 hud_detects <- readRDS('data and imports/hud_detects.rds')
 
-recats <- read.csv('manuscript/recategorized.csv')
+recats <- read.csv('manuscript/recategorized_rkm.csv')
 recats <- recats %>%
-  mutate(
-         pred18 = factor(pred18, levels = c('Upper', 'Lower'), ordered = T))
+  mutate(pred18 = factor(pred18, levels = c('Upper', 'Lower'), ordered = T))
 
 hud_detects <- hud_detects %>%
   left_join(recats, by = 'transmitter') %>%
@@ -25,35 +24,37 @@ hud_detects <- hud_detects %>%
 reduced_pts <- distinct(hud_detects, cluster17, date.floor, array, .keep_all = T)
 
 library(ggplot2)
-fig7 <- ggplot() +
+kneebone <-
+  ggplot() +
   geom_point(data = reduced_pts,
-             aes(x = array, y = doy, color = cluster17),
+             aes(x = array, y = date.floor, color = cluster17),
              position = position_dodge(width = 0.5)) +
   # position_dodge() only works horizontally; have to plot x on y and coord_flip
   coord_flip() +
   # then scale the y axis since it's flipped
-  scale_y_continuous(expand = c(0.005, 0)) +
+  scale_y_datetime(limits = c(lubridate::ymd('2016-01-01', tz = 'America/New_York'),
+                              lubridate::ymd('2018-12-31', tz = 'America/New_York')),
+                   expand = c(0,7*24*3600)) +
   scale_color_manual(values = c('gray', '#7DC6D8','#FF7762'),
                      guide = guide_legend(reverse = T)) +
-  facet_wrap(~ year) +
-  labs(x = NULL, y = 'Day of year', color = "2017 Spawning region") +
+  labs(x = NULL, y = NULL, color = "2017 Spawning region") +
   theme_bw() +
   theme(legend.position = c(0.02, 0.4),
         legend.justification = c(0, 1),
         legend.text = element_text(size = 10),
         legend.title = element_text(size = 10),
-        strip.background = element_rect(fill = NA),
-        strip.text = element_text(size = 12),
-        # axis.text.y.left = element_text(angle = 35),
         axis.text.y = element_text(size = 8),
         axis.title.x=element_text(size = 10),
         axis.text.x=element_text(size = 8),
-        strip.background.x = element_blank(),
-        strip.text.x = element_text(size = 12),
-        plot.margin = unit(c(0.2, 0.2, 0.1, 0.05), "cm"),
-        panel.spacing.x = unit(0.01, "lines"))
+        plot.margin = margin(0, 7, 0, 0))
 
 
-ggsave("manuscript/submission/figures/Figure7.tif", fig7,
-       width = 7.5, height = 3.5, units = 'in',
-       device = 'tiff', compression = 'lzw')
+
+
+
+library(ragg)
+agg_tiff("p:/obrien/biotelemetry/hudson sb/hud-sb-analysis/manuscript/revision/figures/Figure7.tif",
+         width = 7.5, height = 3.5, units = 'in', res = 600, compression = 'lzw')
+fig7
+
+dev.off()
