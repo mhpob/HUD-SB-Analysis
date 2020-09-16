eed95e2c04dd309ea815bc031ce8b4071da2caff
Mike O'Brien <mikeob9@gmail.com>
2020-09-16 15:58:13 -0400

    Add map.

--- Manuscript/kneebone_manu.R
+++ Manuscript/kneebone_manu.R
@@ -28,13 +28,14 @@ kneebone <-
   ggplot() +
   geom_point(data = reduced_pts,
              aes(x = array, y = date.floor, color = cluster17),
-             position = position_dodge(width = 0.5)) +
+             position = position_dodge(width = 0.5), size = 0.5) +
   # position_dodge() only works horizontally; have to plot x on y and coord_flip
   coord_flip() +
   # then scale the y axis since it's flipped
   scale_y_datetime(limits = c(lubridate::ymd('2016-01-01', tz = 'America/New_York'),
                               lubridate::ymd('2018-12-31', tz = 'America/New_York')),
-                   expand = c(0,7*24*3600)) +
+                   expand = c(0,7*24*3600),
+                   position = 'right') +
   scale_color_manual(values = c('gray', '#7DC6D8','#FF7762'),
                      guide = guide_legend(reverse = T)) +
   labs(x = NULL, y = NULL, color = "2017 Spawning region") +
@@ -46,15 +47,43 @@ kneebone <-
         axis.text.y = element_text(size = 8),
         axis.title.x=element_text(size = 10),
         axis.text.x=element_text(size = 8),
-        plot.margin = margin(0, 7, 0, 0))
+        plot.margin = margin(0, 0, 0, 0))
 
 
 
+library(sf)
+coast <- st_read('p:/obrien/natural earth/ne_50m_land/ne_50m_land.shp') %>%
+  st_crop(xmin = -77, xmax = -69, ymin = 36, ymax = 44)
+lake <- st_read('p:/obrien/natural earth/ne_50m_lakes/ne_50m_lakes.shp')
+base_map <- st_difference(st_union(coast), st_union(lake))
+
+recs <- hud_detects %>%
+  distinct(year, lat, long)
+
+map <-
+  ggplot() +
+  geom_sf(data = base_map, fill  = 'grey', color = 'lightgray') +
+  coord_sf(xlim = c(-77, -69), ylim = c(36.53, 44), expand = F,
+           label_axes = '-NE-') +
+  scale_x_continuous(breaks = seq(-76, -70, 2)) +
+  scale_y_continuous(breaks = seq(37, 44, 2)) +
+  theme_bw() +
+  geom_point(data = recs, aes(x = long, y = lat), size = 0.5) +
+  facet_wrap(~ year) +
+  labs(x = NULL, y = NULL) +
+  theme(strip.text = element_blank(),
+        strip.background = element_blank(),
+        panel.spacing.x = unit(2, 'points'),
+        plot.margin = margin(0, 0, 0, 0))
 
 
-library(ragg)
-agg_tiff("p:/obrien/biotelemetry/hudson sb/hud-sb-analysis/manuscript/revision/figures/Figure7.tif",
-         width = 7.5, height = 3.5, units = 'in', res = 600, compression = 'lzw')
+library(patchwork)
+fig7 <- kneebone / map
+
+
+tiff("p:/obrien/biotelemetry/hudson sb/hud-sb-analysis/manuscript/revision/figures/Figure7.tif",
+         width = 7, height = 6, units = 'in', res = 600, compression = 'lzw')
 fig7
 
 dev.off()
+
